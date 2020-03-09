337b3604814e801f49d17060862bcbac49b8fa13
Talal Abbas <talalabbas84@gmail.com>
2020-03-10 01:35:40 +0500

    added env port

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ mongoose
   )
   .then(() => {
     console.log('Mongodb connect');
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
   })
   .catch(err => {
     console.log(err);
