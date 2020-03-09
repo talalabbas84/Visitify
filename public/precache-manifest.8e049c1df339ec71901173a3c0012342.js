6079c7de3487260bb045293cefaba98bb146731f
Talal Abbas <talalabbas84@gmail.com>
2020-03-10 02:33:01 +0500

    added new react app frontend version

---
+++ public/precache-manifest.8e049c1df339ec71901173a3c0012342.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "064e22cc2176d20a5e44a273a1a85570",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77d12cb53fd6f1e942da",
+    "url": "/static/css/0.8c244f1a.chunk.css"
+  },
+  {
+    "revision": "6aa18e2c398aa4d91be1",
+    "url": "/static/css/4.a2d54d2f.chunk.css"
+  },
+  {
+    "revision": "535d08ead96bd2151039",
+    "url": "/static/css/5.2e37bd02.chunk.css"
+  },
+  {
+    "revision": "0c6f3f3daaecdddce17a",
+    "url": "/static/css/6.021b4897.chunk.css"
+  },
+  {
+    "revision": "c4d4a13ea8bf5063ae5f",
+    "url": "/static/css/7.d06a00af.chunk.css"
+  },
+  {
+    "revision": "a1d0253e941b693b6cd5",
+    "url": "/static/css/8.383ac5fa.chunk.css"
+  },
+  {
+    "revision": "3bd3ff3258f57f84aaee",
+    "url": "/static/css/main.3890c020.chunk.css"
+  },
+  {
+    "revision": "77d12cb53fd6f1e942da",
+    "url": "/static/js/0.d3ee4067.chunk.js"
+  },
+  {
+    "revision": "447624e052eaf09f4085",
+    "url": "/static/js/3.59845955.chunk.js"
+  },
+  {
+    "revision": "6aa18e2c398aa4d91be1",
+    "url": "/static/js/4.20f9ff32.chunk.js"
+  },
+  {
+    "revision": "535d08ead96bd2151039",
+    "url": "/static/js/5.3fd6b014.chunk.js"
+  },
+  {
+    "revision": "0c6f3f3daaecdddce17a",
+    "url": "/static/js/6.73392eb9.chunk.js"
+  },
+  {
+    "revision": "c4d4a13ea8bf5063ae5f",
+    "url": "/static/js/7.37033a51.chunk.js"
+  },
+  {
+    "revision": "a1d0253e941b693b6cd5",
+    "url": "/static/js/8.1f662652.chunk.js"
+  },
+  {
+    "revision": "3bd3ff3258f57f84aaee",
+    "url": "/static/js/main.b67eaf54.chunk.js"
+  },
+  {
+    "revision": "0f667c4b0159e4adfbe5",
+    "url": "/static/js/runtime-main.c7d1265d.js"
+  }
+]);
